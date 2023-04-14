969214b4510a4006a020eb24f8fce7c5856434db
data <data.shkubuliani@redberry.ge>
2023-04-15 00:04:45 +0400

    style: update markup

--- resources/views/components/form/field.blade.php
+++ resources/views/components/form/field.blade.php
@@ -1,3 +1,3 @@
-<div class="mb-4">
+<div class="mb-6">
     {{ $slot }}
 </div>
--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -1,5 +1,11 @@
 @props(['name'])
 
 <label for="{{ $name }}" class="font-semibold block mb-1">
-    {{ ucwords($name) }}
+    @if($name === 'new_password')
+        New password
+    @elseif($name === 'repeated_password')
+        Repeated password
+    @else
+        {{ ucwords($name) }}
+    @endif
 </label>
