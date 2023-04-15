b6ade5ab770eeccb5bc583eca19165d7818115fc
data <data.shkubuliani@redberry.ge>
2023-04-16 03:17:07 +0400

    style: update markup

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
     <title>Coronatime</title>
 </head>
 <body class="min-h-screen">
--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes(['class' => 'flex items-center mb-10']) }}>
+<div {{ $attributes(['class' => 'flex items-center']) }}>
     <img src="./images/green-ellipse.png" alt="green half elippse">
     <img class="-ml-2" src="./images/yellow-ellipse.png" alt="green half elippse">
     <h1 class="ml-0.5 text-indigo-700 text-3xl font-semibold mb-1.5">ronatime</h1>
