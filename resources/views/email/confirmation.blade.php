9746e6602f664ff14827497d80ca31d219dadb2c
data <data.shkubuliani@redberry.ge>
2023-04-15 17:36:48 +0400

    style: create new view for email confirmation hint

---
+++ resources/views/email/confirmation.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <div class="w-fit flex flex-col items-center py-6 m-auto">
+        <x-logo/>
+
+        <div class="flex flex-col items-center mt-28 sm:mt-44">
+            <img src="./images/checkmark.gif">
+            <p class="mt-2 text-lg text-center">We have sent you a confirmation email</p>
+        </div>
+    </div>
+</x-layout>
