d6644490599b9926ed0a51721ebab80912e1fab9
data <data.shkubuliani@redberry.ge>
2023-04-20 01:05:34 +0400

    style: create new blade components and update header and logo components

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,10 +1,10 @@
-<header class="flex items-center justify-between">
-    <x-logo class="text-black"/>
+<header class="flex items-center justify-between border-b-2 border-slate-50 md:px-20 p-4">
+    <x-logo color="text-teal-400"/>
     <div class="flex">
         <div x-data="{ show: false }" class="relative px-2 cursor-pointer">
             <div class="flex items-center" @click="show = !show">
                 <span>English</span>
-                <img class="ml-2" src="./images/down-arrow.png">
+                <img class="ml-2" src="{{ asset('assets/icons/down-arrow.png') }}">
             </div>
             <div x-show="show" class="absolute text-sm left-0 top-8 w-full text-white flex flex-col bg-indigo-500 rounded">
                 <a @click="show = !show" class="p-1 text-center hover:bg-indigo-700 duration-300 hover:rounded" href="#">
@@ -13,7 +13,10 @@
             </div>
         </div>
 
-        <h1 class="font-semibold mx-4 border-r-2 pr-4">Davit Shkubuliani</h1>
-        <a href="#">Log Out</a>
+        <div class="hidden sm:flex">
+            <h1 class="font-semibold mx-4 border-r-2 pr-4">Davit Shkubuliani</h1>
+            <a href="#">Log Out</a>
+        </div>
+        <img class="ml-4 w-6 sm:hidden" src="{{ asset('assets/icons/menu-bar.png') }}" alt="menu bar">
     </div>
 </header>
---
+++ resources/views/components/statistics/worldwide.blade.php
@@ -0,0 +1,26 @@
+<div class="w-full grid gap-5 grid-cols-2 md:grid-cols-3 px-3 md:px-0 mt-8 md:mt-16">
+    <div class="flex flex-col items-center p-10 bg-indigo-100 rounded-xl w-full col-start-1 col-end-3 md:col-end-2">
+        <div class="relative mb-6">
+            <img src="{{ asset('assets/images/blue-vector1.png') }}">
+            <img class="absolute top-0" src="{{ asset('assets/images/blue-vector2.png') }}">
+        </div>
+        <h2 class="mb-4 text-lg font-medium">New cases</h2>
+        <h1 class="font-semibold text-4xl text-indigo-700">715,523</h1>
+    </div>
+    <div class="flex flex-col items-center p-10 bg-green-100 rounded-xl w-full">
+        <div class="relative mb-8">
+            <img src="{{ asset('assets/images/green-vector1.png') }}">
+            <img class="absolute top-0" src="{{ asset('assets/images/green-vector2.png') }}">
+        </div>
+        <h2 class="mb-4 text-lg font-medium">Recovered</h2>
+        <h1 class="font-semibold text-4xl text-green-500">72,005</h1>
+    </div>
+    <div class="flex flex-col items-center p-10 bg-amber-50 rounded-xl w-full">
+        <div class="relative mb-8">
+            <img src="{{ asset('assets/images/yellow-vector1.png') }}">
+            <img class="absolute top-0" src="{{ asset('assets/images/yellow-vector2.png') }}">
+        </div>
+        <h2 class="mb-4 text-lg font-medium">Death</h2>
+        <h1 class="font-semibold text-4xl text-amber-300">8,332</h1>
+    </div>
+</div>
