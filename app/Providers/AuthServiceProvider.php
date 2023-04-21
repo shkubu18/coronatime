72b02a992c9e9d341ebb27544fcd881f83789c1a
data <data.shkubuliani@redberry.ge>
2023-04-22 00:41:02 +0400

    feat: send email confirmation link to users

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,24 +3,30 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-    ];
+	/**
+	 * The model to policy mappings for the application.
+	 *
+	 * @var array<class-string, class-string>
+	 */
+	protected $policies = [
+		// 'App\Models\Model' => 'App\Policies\ModelPolicy',
+	];
 
-    /**
-     * Register any authentication / authorization services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+	/**
+	 * Register any authentication / authorization services.
+	 */
+	public function boot(): void
+	{
+		VerifyEmail::toMailUsing(function ($notifiable, $url) {
+			return (new MailMessage)
+				->view('emails.verify-email', ['url' => $url])
+				->subject('Verify your email address');
+		});
+	}
 }
