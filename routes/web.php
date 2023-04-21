a34c93f42a524d0ae29d3dc75d8f6f8c79073b0b
data <data.shkubuliani@redberry.ge>
2023-04-22 00:18:51 +0400

    feat: register new routes for registration and email verification

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\EmailVerificationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-	return view('welcome');
-});
+Route::view('/registration', 'register.create')->name('register.page');
+
+Route::post('/registration', [RegistrationController::class, 'createUser'])->name('register.create');
+
+Route::view('/email/confirmation-sent', 'verifications.email-confirmation-sent')->name('email.confirmation_sent');
+
+Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])->name('verification.verify');
+
+Route::view('/email/verified', 'verifications.email-verified')->name('verification.notice');
