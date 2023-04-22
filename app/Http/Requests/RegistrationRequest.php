628f17d3ab42c317fb471e1d149800284c9e5c76
data <data.shkubuliani@redberry.ge>
2023-04-22 19:15:44 +0400

    chore: update repeated password name and also remove unneccesary rule

--- app/Http/Requests/RegistrationRequest.php
+++ app/Http/Requests/RegistrationRequest.php
@@ -9,10 +9,10 @@ class RegistrationRequest extends FormRequest
 	public function rules(): array
 	{
 		return [
-			'username'             => ['required', 'string', 'min:3', 'unique:users'],
-			'email'                => ['required', 'email', 'unique:users'],
-			'password'             => ['required', 'min:3'],
-			'repeated_password'    => ['required_with:password', 'min:3', 'same:password'],
+			'username'                      => ['required', 'string', 'min:3', 'unique:users'],
+			'email'                         => ['required', 'email', 'unique:users'],
+			'password'                      => ['required', 'min:3'],
+			'password_confirmation'         => ['required_with:password', 'same:password'],
 		];
 	}
 }
