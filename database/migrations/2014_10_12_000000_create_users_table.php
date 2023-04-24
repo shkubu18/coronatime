a8bfb4f1d9aa0d0b1129c08f662c1ca792d0e5a8
data <data.shkubuliani@redberry.ge>
2023-04-24 17:08:15 +0400

    chore: add email verification token column in users table and set also as index column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,13 @@ return new class() extends Migration {
 			$table->id();
 			$table->string('username')->unique();
 			$table->string('email')->unique();
+			$table->string('email_verification_token')->unique();
 			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password');
 			$table->rememberToken();
 			$table->timestamps();
+
+			$table->index('email_verification_token');
 		});
 	}
 
