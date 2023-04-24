d3029fefe393fea88fcf7fed2dc2e23e98b073a1
data <data.shkubuliani@redberry.ge>
2023-04-24 17:55:46 +0400

    chore: remove unneccesary routes for this moment

--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,6 @@ use App\Http\Controllers\EmailVerificationController;
 |
 */
 
-Route::get('/', function () {
-	return redirect('/login');
-});
-
-Route::view('/login', 'auth.login')->name('login.page');
-
-Route::middleware('auth')->group(function () {
-	Route::view('/statistics/worldwide', 'user.dashboard.worldwide')->name('dashboard.worldwide');
-	Route::view('/statistics/by-country', 'user.dashboard.by-country')->name('dashboard.by_country');
-});
-
-Route::view('/reset-password', 'reset-password.email-verify')->name('password.verify_email');
-Route::view('/new-password', 'reset-password.reset')->name('password.reset');
-Route::view('/password-updated', 'hints.password-updated')->name('password.updated');
-
 Route::view('/registration', 'register.create')->name('register.page');
 
 Route::post('/registration', [RegistrationController::class, 'createUser'])->name('register.create');
