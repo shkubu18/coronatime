f6eb1d08cc6ae1c44d3db56c2a39eca86ed513fd
data <data.shkubuliani@redberry.ge>
2023-04-24 22:43:14 +0400

    fix: update input name,id and for attribute name for label

--- resources/views/components/form/remember-device.blade.php
+++ resources/views/components/form/remember-device.blade.php
@@ -3,10 +3,10 @@
         <input
             class="w-5 h-5 rounded border-2 border-gray-200 mr-2"
             type="checkbox"
-            name="remember_device"
-            id="remember_device"
+            name="remember"
+            id="remember"
         >
-        <label class="font-medium" for="remember_device">Remember this device</label>
+        <label class="font-medium" for="remember">Remember this device</label>
     </div>
     <a class="text-indigo-700 font-medium" href="{{ route('password.verify_email') }}">Forgot password?</a>
 </div>
