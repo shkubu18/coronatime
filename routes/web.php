c4968d7c8c6df745032c042f6b8cdfd90071ae32
data <data.shkubuliani@redberry.ge>
2023-04-24 17:52:37 +0400

    refactor: group routes with similar prefixes

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,27 @@ use App\Http\Controllers\EmailVerificationController;
 |
 */
 
+Route::get('/', function () {
+	return redirect('/login');
+});
+
+Route::view('/login', 'auth.login')->name('login.page');
+
+Route::middleware('auth')->group(function () {
+	Route::view('/statistics/worldwide', 'user.dashboard.worldwide')->name('dashboard.worldwide');
+	Route::view('/statistics/by-country', 'user.dashboard.by-country')->name('dashboard.by_country');
+});
+
+Route::view('/reset-password', 'reset-password.email-verify')->name('password.verify_email');
+Route::view('/new-password', 'reset-password.reset')->name('password.reset');
+Route::view('/password-updated', 'hints.password-updated')->name('password.updated');
+
 Route::view('/registration', 'register.create')->name('register.page');
 
 Route::post('/registration', [RegistrationController::class, 'createUser'])->name('register.create');
 
-Route::view('/email/confirmation-sent', 'verifications.email-confirmation-sent')->name('email.confirmation_sent');
-
-Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])->name('verification.verify');
-
-Route::view('/email/verified', 'verifications.email-verified')->name('verification.notice');
+Route::prefix('email')->group(function () {
+	Route::view('confirmation-sent', 'verifications.email-confirmation-sent')->name('email.confirmation_sent');
+	Route::get('verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])->name('verification.verify');
+	Route::view('verified', 'verifications.email-verified')->name('verification.notice');
+});
