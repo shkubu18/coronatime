fd2ed0a1a57fe957d74d32c890ce25391ae4a08c
data <data.shkubuliani@redberry.ge>
2023-04-25 18:16:48 +0400

    feat: create view for password reset email layout

---
+++ resources/views/emails/password-reset.blade.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coronatime</title>
+</head>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;900&display=swap');
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Inter', sans-serif;
+    }
+
+    body {
+        min-height: 100vh;
+        display: grid;
+        align-items: center;
+        justify-content: center;
+    }
+
+    div {
+        max-width: 520px;
+        min-width: 300px;
+        padding: 5px;
+        text-align: center;
+    }
+
+    img {
+        width: 100%;
+    }
+
+    h1 {
+        font-size: 25px;
+        margin-top: 45px;
+        margin-bottom: 15px;
+    }
+
+    h3 {
+        font-weight: 400;
+        margin-bottom: 35px;
+    }
+
+    a {
+        background-color: #0FBA68;
+        display: inline-block;
+        color: white;
+        padding: 12px;
+        width: 80%;
+        border-radius: 6px;
+        font-weight: 900;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+
+    @media screen and (max-width: 500px) {
+        a {
+            width: 100%;
+        }
+    }
+</style>
+<body>
+<div>
+    <img src="{{ asset('./assets/images/image-for-emails.png') }}" alt="dashboard">
+    <h1>Recover password</h1>
+    <h3>click this button to recover password</h3>
+    <br>
+    <a href="{{ route('password.reset', $token) }}">Recover Password</a>
+</div>
+</body>
+</html>
