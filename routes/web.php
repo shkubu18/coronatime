57501d2b5fa5e987b3d481b2645ff4e66b037458
data <data.shkubuliani@redberry.ge>
2023-04-25 18:34:41 +0400

    feat: register new routes for password resetting * reformat routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\EmailVerificationController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ResetPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,20 +22,30 @@ Route::get('/', function () {
 });
 
 Route::middleware('guest')->group(function () {
+	// registration
+	Route::view('/registration', 'register.create')->name('register.page');
+	Route::post('/registration', [RegistrationController::class, 'createUser'])->name('register.create');
+
+	// email verification
+	Route::prefix('email')->group(function () {
+		Route::view('confirmation-sent', 'verifications.email-confirmation-sent')->name('email.confirmation_sent');
+		Route::get('verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])->name('verification.verify');
+		Route::view('verified', 'verifications.email-verified')->name('verification.notice');
+	});
+
+	// authorization
 	Route::view('/login', 'auth.login')->name('login.page');
 	Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+	// password reset
+	Route::view('/forgot-password', 'reset-password.forgot-password')->name('password.request');
+	Route::post('/forgot-password', [ResetPasswordController::class, 'sendResetLink'])->name('password.email');
+	Route::get('/reset-password/{token}', [ResetPasswordController::class, 'showResetPasswordForm'])->name('password.reset');
+	Route::post('/reset-password', [ResetPasswordController::class, 'updatePassword'])->name('password.update');
+	Route::view('/password-updated', 'reset-password.password-updated')->name('password.updated');
 });
 
 Route::middleware('auth')->group(function () {
+	// logout
 	Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-});
-
-Route::view('/registration', 'register.create')->name('register.page');
-
-Route::post('/registration', [RegistrationController::class, 'createUser'])->name('register.create');
-
-Route::prefix('email')->group(function () {
-	Route::view('confirmation-sent', 'verifications.email-confirmation-sent')->name('email.confirmation_sent');
-	Route::get('verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])->name('verification.verify');
-	Route::view('verified', 'verifications.email-verified')->name('verification.notice');
 });
