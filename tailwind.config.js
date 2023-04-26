9b3d874300eccec24f9ca1be294945719bd9b9a8
data <data.shkubuliani@redberry.ge>
2023-04-26 19:58:01 +0400

    chore: require tailwind scrollbar plugin * update responsive brakepoints * add new costum height

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,21 @@ module.exports = {
         "./resources/**/*.blade.php",
     ],
     theme: {
-        extend: {},
+        extend: {
+            height: {
+                '600': '600px',
+            },
+        },
+        screens: {
+            'xs': '370px',
+            'sm': '576px',
+            'md': '960px',
+            'lg': '1440px',
+        }
     },
     plugins: [
         require('@tailwindcss/forms'),
+        require('tailwind-scrollbar'),
     ],
 }
 
