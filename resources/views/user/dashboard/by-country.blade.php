1e9560e13f9d614a2135c40eb7c144733dfc8f68
data <data.shkubuliani@redberry.ge>
2023-04-26 20:12:53 +0400

    feat: update search input * pass countires and total numbers to component

--- resources/views/user/dashboard/by-country.blade.php
+++ resources/views/user/dashboard/by-country.blade.php
@@ -4,14 +4,19 @@
         <h1 class="text-2xl font-semibold pl-3 md:pl-0">Statistics by country</h1>
         <x-nav />
         <div class="relative my-3 md:my-10 md:w-72">
-            <input
-                class="rounded-xl p-3 pl-12 md:pl-16 w-full border-0 md:border-2 md:border-gray-100"
-                type="text"
-                name="search"
-                placeholder="Search by country"
-            >
+            <form method="GET" action="{{ route('dashboard.by_country') }}">
+                <input class="rounded-xl p-3 pl-12 md:pl-16 w-full border-0 md:border-2 md:border-gray-100"
+                       type="text"
+                       name="search"
+                       placeholder="Search by country"
+                       value="{{ request()->query('search') }}"
+                >
+            </form>
             <img class="absolute top-3.5 sm:top-4 left-4 sm:left-6" src="{{ asset('assets/icons/search-icon.png') }}" alt="search icon">
         </div>
-        <x-statistics.by-country />
+        <x-statistics.by-country
+            :countries="$countries"
+            :totalNumbers="$totalNumbers"
+        />
     </div>
 </x-layout>
