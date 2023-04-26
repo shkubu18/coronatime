e5e7c7bb95f2723a0445196f38e2382b7757691f
data <data.shkubuliani@redberry.ge>
2023-04-26 19:46:16 +0400

    feat: create new model and table for covid statistics

---
+++ app/Models/CovidStatistic.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CovidStatistic extends Model
+{
+	use HasFactory;
+
+	protected $guarded = ['id'];
+}
