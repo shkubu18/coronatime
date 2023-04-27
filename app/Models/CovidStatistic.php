a42b9fe053078db6d1814127ea8dd3fc27f8c86d
data <data.shkubuliani@redberry.ge>
2023-04-27 15:47:58 +0400

    feat: add new scoupes for searching and sorting logic

--- app/Models/CovidStatistic.php
+++ app/Models/CovidStatistic.php
@@ -10,4 +10,15 @@ class CovidStatistic extends Model
 	use HasFactory;
 
 	protected $guarded = ['id'];
+
+	public function scopeSearch($query, $search)
+	{
+		$search ? $query->where('country->en', 'like', '%' . ucwords($search) . '%')
+			->orWhere('country->ka', 'like', '%' . $search . '%') : $query;
+	}
+
+	public function scopeSort($query, $sortBy, $sortOrder)
+	{
+		return $query->orderBy(in_array($sortBy, ['confirmed', 'deaths', 'recovered']) ? $sortBy : 'id', $sortOrder);
+	}
 }
