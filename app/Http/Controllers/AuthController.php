a8f4473129f6c1c7a58b78042da441ed4fcc6ad4
data <data.shkubuliani@redberry.ge>
2023-04-30 09:04:27 +0400

    refactor: update login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,23 +11,18 @@ class AuthController extends Controller
 {
 	public function login(LoginRequest $request): RedirectResponse
 	{
-		$loginType = $request->login_type;
-
-		$remember = $request->remember;
-
-		$user = User::where($loginType, $request->login)->first();
-
-		if (!$user->hasVerifiedEmail())
-		{
-			return throw ValidationException::withMessages([
-				'email_verify' => 'Your email is not verified. Please verify your email before logging in',
-			]);
-		}
-
-		if (!auth()->attempt([$loginType => $request->login, 'password' => $request->password], $remember))
+		if (!auth()->attempt([$request->login_type => $request->login, 'password' => $request->password], $request->remember))
 		{
 			throw ValidationException::withMessages([
 				'auth_fail' => 'Email or password is incorrect.',
+			]);
+		}
+
+		if (!User::where($request->login_type, $request->login)->first()->hasVerifiedEmail())
+		{
+			auth()->logout();
+			throw ValidationException::withMessages([
+				'email_verify' => 'Your email is not verified. Please verify your email before logging in',
 			]);
 		}
 
