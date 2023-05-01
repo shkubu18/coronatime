17ed80aa1d8fcdf3c97da7af930db61c311a50b0
data <data.shkubuliani@redberry.ge>
2023-05-02 01:33:17 +0400

    test: add tests for covid statistic pages

---
+++ tests/Feature/CovidStatisticTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\CovidStatistic;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CovidStatisticTest extends TestCase
+{
+	use RefreshDatabase;
+
+	public function test_covid_statistic_should_return_statistics_worldwide_page_with_total_numbers(): void
+	{
+		$user = User::factory()->create();
+
+		CovidStatistic::create([
+			'country_code' => 'US',
+			'country'      => '{"en":"World","ka":"აშშ"}',
+			'confirmed'    => 100,
+			'recovered'    => 50,
+			'deaths'       => 10,
+		]);
+
+		$response = $this->actingAs($user)->get('/statistics/worldwide');
+
+		$response->assertStatus(200);
+
+		$response->assertViewHas('totalNumbers');
+	}
+
+	public function test_covid_statistics_should_return_statistics_by_country_page_with_all_countries_and_total_numbers()
+	{
+		$user = User::factory()->create();
+
+		CovidStatistic::create([
+			'country_code' => 'US',
+			'country'      => '{"en":"United States","ka":"\u10d0\u10e8\u10e8"}',
+			'confirmed'    => 100,
+			'recovered'    => 80,
+			'deaths'       => 10,
+		]);
+
+		$response = $this->actingAs($user)->get('/statistics/by-country');
+
+		$response->assertStatus(200);
+
+		$response->assertViewHas('totalNumbers');
+
+		$response->assertSee('United States');
+		$response->assertSee('100');
+		$response->assertSee('80');
+		$response->assertSee('10');
+	}
+}
