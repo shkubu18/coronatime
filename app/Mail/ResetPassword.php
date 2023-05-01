2347b4d56dadd2f696650e1f4a46baba88e10bec
data <data.shkubuliani@redberry.ge>
2023-05-01 21:37:39 +0400

    feat: add new mailer classfor reseting passwords

---
+++ app/Mail/ResetPassword.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ResetPassword extends Mailable
+{
+	use Queueable, SerializesModels;
+
+	use Queueable, SerializesModels;
+
+	/**
+	 * The password reset token.
+	 *
+	 * @var string
+	 */
+	public $token;
+
+	/**
+	 * Create a new message instance.
+	 *
+	 * @param string $token
+	 *
+	 * @return void
+	 */
+	public function __construct($token)
+	{
+		$this->token = $token;
+	}
+
+	/**
+	 * Build the message.
+	 *
+	 * @return $this
+	 */
+	public function build()
+	{
+		return $this->view('emails.password-reset')
+			->subject('Reset Password');
+	}
+}
