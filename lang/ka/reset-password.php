13ec8ee7bc1add57f6389616d5866b7495e17efb
data <data.shkubuliani@redberry.ge>
2023-05-03 14:50:50 +0400

    chore: add new translatable text for resetting passwords

--- lang/ka/reset-password.php
+++ lang/ka/reset-password.php
@@ -1,13 +1,14 @@
 <?php
 
 return [
-	'reset_password'        => 'პაროლის აღდგენა',
-	'email'                 => 'ელ-ფოსტა',
-	'email_placeholder'     => 'შეიყვანეთ ელ-ფოსტა',
-	'password'              => 'ახალი პაროლი',
-	'password_placeholder'  => 'შეიყვანეთ ახალი პაროლი',
-	'password_confirmation' => 'გაიმეორეთ პაროლი',
-	'save_changes'          => 'ცვლილებების შენახვა',
-	'password_updated'      => 'თქვენი პაროლი წარმატებით განახლდა',
-	'login'                 => 'შესვლა',
+	'reset_password'            => 'პაროლის აღდგენა',
+	'email'                     => 'ელ-ფოსტა',
+	'email_placeholder'         => 'შეიყვანეთ ელ-ფოსტა',
+	'password'                  => 'ახალი პაროლი',
+	'password_placeholder'      => 'შეიყვანეთ ახალი პაროლი',
+	'password_confirmation'     => 'გაიმეორეთ პაროლი',
+	'save_changes'              => 'ცვლილებების შენახვა',
+	'password_updated'          => 'თქვენი პაროლი წარმატებით განახლდა',
+	'login'                     => 'შესვლა',
+	'reset_link_already_sended' => 'თქვენ უკვე მოითხოვეთ პაროლის აღდგენის ბმული. გთხოვთ შეამოწმოთ თქვენი ელ-ფოსტა',
 ];
