e5d495d2c0c238507b30d17cba949c679a244bf3
data <data.shkubuliani@redberry.ge>
2023-05-03 19:03:41 +0400

    chore: add return type declaration to methods

--- app/Models/CovidStatistic.php
+++ app/Models/CovidStatistic.php
@@ -11,12 +11,29 @@ class CovidStatistic extends Model
 
 	protected $guarded = ['id'];
 
+	/**
+	 * Scope a query to search for a given term in the "en" and "ka" fields of the "country" JSON column.
+	 *
+	 * @param \Illuminate\Database\Eloquent\Builder $query
+	 * @param string|null                           $search
+	 *
+	 * @return \Illuminate\Database\Eloquent\Builder
+	 */
 	public function scopeSearch($query, $search)
 	{
-		$search ? $query->where('country->en', 'like', '%' . ucwords($search) . '%')
+		return $search ? $query->where('country->en', 'like', '%' . ucwords($search) . '%')
 			->orWhere('country->ka', 'like', '%' . $search . '%') : $query;
 	}
 
+	/**
+	 * Scope a query to sort by a given column and order.
+	 *
+	 * @param \Illuminate\Database\Eloquent\Builder $query
+	 * @param string                                $sortBy
+	 * @param string                                $sortOrder
+	 *
+	 * @return \Illuminate\Database\Eloquent\Builder
+	 */
 	public function scopeSort($query, $sortBy, $sortOrder)
 	{
 		return $query->orderBy(in_array($sortBy, ['confirmed', 'deaths', 'recovered']) ? $sortBy : 'id', $sortOrder);
