e3200a4f19b657fbc2b69441a4f1a4cb7549f9d5
data <data.shkubuliani@redberry.ge>
2023-05-04 20:30:45 +0400

    chore: update covid update artisan command name

--- app/Console/Commands/UpdateCovidStatisticsCommand.php
+++ app/Console/Commands/UpdateCovidStatisticsCommand.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
-class UpdateCovidStatistics extends Command
+class UpdateCovidStatisticsCommand extends Command
 {
 	/**
 	 * The name and signature of the console command.
