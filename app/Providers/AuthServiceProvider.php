d616cce596628f37daedfb82a4752dae85e0d721
data <data.shkubuliani@redberry.ge>
2023-05-05 14:31:24 +0400

    chore: clear boot method in auth service provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,12 +3,7 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
-use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\URL;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,19 +21,5 @@ class AuthServiceProvider extends ServiceProvider
 	 */
 	public function boot(): void
 	{
-		VerifyEmail::toMailUsing(function ($notifiable) {
-			$verificationUrl = URL::temporarySignedRoute(
-				'verification.verify',
-				Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
-				[
-					'id'                            => $notifiable->email_verification_token,
-					'hash'                          => sha1($notifiable->getEmailForVerification()),
-				]
-			);
-
-			return (new MailMessage)
-				->view('emails.verify-email', ['verificationUrl' => $verificationUrl])
-				->subject('Verify your email address');
-		});
 	}
 }
