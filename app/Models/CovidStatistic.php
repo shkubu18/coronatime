d9801ecea2528005946895f3e5075bc3830747cc
data <data.shkubuliani@redberry.ge>
2023-05-06 00:35:49 +0400

    refactor(statistics): add types to methods properties

--- app/Models/CovidStatistic.php
+++ app/Models/CovidStatistic.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class CovidStatistic extends Model
 {
@@ -11,13 +12,13 @@ class CovidStatistic extends Model
 
 	protected $guarded = ['id'];
 
-	public static function scopeSearch($query, $search): void
+	public static function scopeSearch(Builder $query, ?string $search): void
 	{
 		$search ? $query->where('country->en', 'like', '%' . ucwords($search) . '%')
 			->orWhere('country->ka', 'like', '%' . $search . '%') : $query;
 	}
 
-	public static function scopeSort($query, $sortBy, $sortOrder): void
+	public static function scopeSort(Builder $query, string $sortBy, string $sortOrder): void
 	{
 		$query->orderBy(in_array($sortBy, ['confirmed', 'deaths', 'recovered']) ? $sortBy : 'id', $sortOrder);
 	}
