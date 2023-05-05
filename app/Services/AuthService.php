13e7562ba324dd10a84f5b5c7817f86e65c836ef
data <data.shkubuliani@redberry.ge>
2023-05-05 21:34:10 +0400

    feat: add service for authentication logic

---
+++ app/Services/AuthService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+
+class AuthService
+{
+	public static function authenticate(LoginRequest $request)
+	{
+		if (!auth()->attempt([$request->login_type => $request->username_or_email, 'password' => $request->password], $request->remember))
+		{
+			throw ValidationException::withMessages([
+				'auth_fail' => 'Email or password is incorrect',
+			]);
+		}
+	}
+
+	public static function checkEmailVerification(LoginRequest $request)
+	{
+		if (!User::where($request->login_type, $request->username_or_email)->first()->hasVerifiedEmail())
+		{
+			auth()->logout();
+			throw ValidationException::withMessages([
+				'email_verify' => 'Your email is not verified. Please verify your email before logging in',
+			]);
+		}
+	}
+}
