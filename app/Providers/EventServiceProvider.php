0a3716680f8bb9b4e302ee3f45386590c1f8b4eb
data <data.shkubuliani@redberry.ge>
2023-05-05 14:32:54 +0400

    chore: remove Register event from event service provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,9 +3,7 @@
 namespace App\Providers;
 
 use App\Listeners\RedirectVerifiedUser;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Events\Verified;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
@@ -17,9 +15,6 @@ class EventServiceProvider extends ServiceProvider
 	 * @var array<class-string, array<int, class-string>>
 	 */
 	protected $listen = [
-		Registered::class => [
-			SendEmailVerificationNotification::class,
-		],
 		Verified::class => [
 			RedirectVerifiedUser::class,
 		],
