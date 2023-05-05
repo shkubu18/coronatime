e869edb48f4d15f047775e14605dadcc3111918a
data <data.shkubuliani@redberry.ge>
2023-05-05 12:31:10 +0400

    chore(localization): fix language name

--- lang/ka/header.php
+++ lang/ka/header.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	'georgia' => 'ქართული',
-	'english' => 'ინგლისური',
-	'logout'  => 'გამოსვლა',
+	'georgian' => 'ქართული',
+	'english'  => 'ინგლისური',
+	'logout'   => 'გამოსვლა',
 ];
--- resources/views/components/header/language-switcher.blade.php
+++ resources/views/components/header/language-switcher.blade.php
@@ -1,6 +1,6 @@
 <div x-data="{ show: false }" class="relative mr-2 cursor-pointer">
     <div class="flex items-center" @click="show = !show">
-        <span>{{ app()->getLocale() === 'en' ? __('header.english') : __('header.georgia') }}</span>
+        <span>{{ app()->getLocale() === 'en' ? __('header.english') : __('header.georgian') }}</span>
         <img class="ml-2" src="{{ asset('assets/icons/down-arrow.png') }}">
     </div>
     <div x-show="show" style="display: none" class="absolute text-sm left-0 top-8 w-full text-white flex flex-col bg-teal rounded">
@@ -9,7 +9,7 @@
             class="p-1 text-center hover:bg-blue duration-300 hover:rounded"
             href="{{ route('locale', ['language' => app()->getLocale() === 'en' ? 'ka' : 'en']) }}"
         >
-            {{ app()->getLocale() === 'en' ? __('header.georgia') : __('header.english') }}
+            {{ app()->getLocale() === 'en' ? __('header.georgian') : __('header.english') }}
         </a>
     </div>
 </div>
