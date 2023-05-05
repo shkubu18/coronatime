fc93e1d36bdb9cde46fded46820272fccf7de42d
data <data.shkubuliani@redberry.ge>
2023-05-05 20:45:21 +0400

    feat: add error message to register page if email sending reaches limit

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -35,6 +35,13 @@
                             placeholder="{{ __('register.repeat_password') }}"
                         />
 
+                        @error('verify_email_send_fail')
+                        <div class="flex items-center mt-2 ml-1">
+                            <img class="h-4 mr-3" src="{{ asset('./assets/icons/input-warning.png') }}" alt="warning icon">
+                            <p class="text-warning-color">{{ __($message) }}</p>
+                        </div>
+                        @enderror
+
                         <x-form.button>{{ __('register.sign_up') }}</x-form.button>
                     </form>
                     <div class="flex justify-center">
