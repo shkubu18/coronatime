099c395db7e1df99b88cbb878bc3620173f7004d
data <data.shkubuliani@redberry.ge>
2023-05-05 20:46:16 +0400

    feat(emails): add new translatable message

--- lang/en/sending-emails.php
+++ lang/en/sending-emails.php
@@ -3,6 +3,7 @@
 return [
 	'email_confirmation'      => 'Confirmation email',
 	'email_confirmation_send' => 'We have sent you a confirmation email',
+	'email_confirmation_fail' => 'Failed to send verification email. Please try again later',
 	'email_verify'            => 'click this button to verify your email',
 	'email_verify_btn'        => 'VERIFY EMAIL',
 	'account_verified'        => 'Your account is confirmed, you can sign in',
