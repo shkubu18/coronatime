3de6fdceffa7ee857268d1af646dec1ef56d7c7e
data <data.shkubuliani@redberry.ge>
2023-05-10 16:23:44 +0400

    style: update responsive styles in login page

--- resources/views/components/form/remember-device.blade.php
+++ resources/views/components/form/remember-device.blade.php
@@ -1,12 +1,18 @@
 <div class="flex justify-between text-sm">
-    <div class="flex">
+    <div class="flex items-center">
         <input
             class="w-5 h-5 rounded border-2 border-light-gray mr-2 text-input-green focus:ring-input-green"
             type="checkbox"
             name="remember"
             id="remember"
         >
-        <label class="font-medium" for="remember">{{ __('auth.remember_device') }}</label>
+        <label class="font-medium {{ app()->getLocale() === 'ka' ? 'text-xs sm:text-sm' : ''}}" for="remember">
+            {{ __('auth.remember_device') }}
+        </label>
     </div>
-    <a class="text-blue font-medium" href="{{ route('password.request') }}">{{ __('auth.forgot_password') }}</a>
+    <div class="flex items-center">
+        <a class="text-right ml-2 text-blue font-bold {{ app()->getLocale() === 'ka' ? 'text-xs sm:text-sm' : ''}}" href="{{ route('password.request') }}">
+            {{ __('auth.forgot_password') }}
+        </a>
+    </div>
 </div>
